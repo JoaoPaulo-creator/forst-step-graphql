63993f80ca2def9dcf41418b9d6f88c1a12b5e49
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-04 21:35:17 -0300

    refactor: Adicionado resolver

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import path from "node:path";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { AppointMentsResolver } from "./resolvers/appointments-resolver";
+import { CustomerResolver } from "./resolvers/customer-resolver";
 
 async function main() {
   const schema = await buildSchema({
-    resolvers: [AppointMentsResolver],
+    resolvers: [AppointMentsResolver, CustomerResolver],
     emitSchemaFile: path.resolve(__dirname, "schema.gql"),
   });
 
